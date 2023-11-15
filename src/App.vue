b13f876bb2a84c2fc9d46ac873a8974e523be3c3
BagiPeter30 <128401801+BagiPeter30@users.noreply.github.com>
2023-11-15 14:11:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Calculator from './Components/CalculatorClone.vue';
+</script>
+
+<template>
+  <main>
+    <Calculator />
+  </main>
+
+</template>
+
+<style>
+ body
+{
+  background-color: rgb(38, 38, 185);
+}
+</style>
